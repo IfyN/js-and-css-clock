76e9f458ad9556ba3fcdd1b7e4c63ef87c11ae8f
IfyN <ifynwosu0@gmail.com>
2024-01-17 15:35:41 +0100

    get time

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 //Get time hands
-let hourHand = document.querySelector(".hour-hand");
-let minHand = document.querySelector(".min-hand");
-let secHand = document.querySelector(".second-hand");
+let hourHand = document.querySelector("#hour");
+let minHand = document.querySelector("#minute");
+let secHand = document.querySelector("#second");
 
+setInterval(() => {
+  const now = new Date();
 
-function getHour (){
-    const now = new Date();
+  seconds = now.getSeconds();
+  minutes = now.getMinutes();
+  hour = now.getHours();
+  /* change minutes to degrees, 1 second is 6 degrees*/
+  secDegree = seconds * 6;
 
-    let hour = now.getHours();
-    
-    let minute = now.getMinutes();
+  /* change minutes to degrees, 1 minute is 6 degrees*/
+  minDegree = minutes * 6;
 
-    let second = now.getSeconds();
+  /* change hour to degrees, 1 hour is 30 degrees*/
+  hourDegree = hour * 30;
 
-}
+  secHand.style.transform = `rotate(${secDegree}deg)`;
+  minHand.style.transform = `rotate(${minDegree}deg)`;
+  hourHand.style.transform = `rotate(${hourDegree}deg)`;
+}, 1000);
